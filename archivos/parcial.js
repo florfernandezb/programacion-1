129e27269b3cee89a170b26e20ba9ca519f4c813
ffernandezbu <florencia.bfernandez@mercadolibre.com>
2021-06-07 20:08:30 -0300

    se avanza con el proyecto, hay que corregir mostrarPistaMayorDuracion()

--- archivos/parcial.js
+++ archivos/parcial.js
@@ -9,13 +9,13 @@ let error = new ErrorMsg();
 let flag = 0;
 let errorFlag = false;
 const campoIncompleto = 'Este campo se cargó vacío';
-let listadoDiscos = [ ];
+let listadoDiscos = [];
 
 // Función Cargar:
 const Cargar = () => {
     let disco = new Disco();
     disco.setIdDisco(validacionEntradaIdDisco('Ingrese el codigo del disco'))
-    
+
     disco.setNombreDisco(validacionEntradaTexto('Ingrese el nombre del disco'))
 
     disco.setInterprete(validacionEntradaTexto('Ingrese el nombre del intérprete'))
@@ -30,7 +30,7 @@ const Cargar = () => {
         )
         disco.guardarPista(pista)
     } while (confirm('Desea ingresar otra pista?'))
-    
+
     listadoDiscos.push(disco)
 
 }
@@ -40,16 +40,23 @@ const Mostrar = () => {
     // Variable para ir armando la cadena:
     let html = ``;
 
-    if(listadoDiscos.length == 0) {
+    if (listadoDiscos.length == 0) {
         html += '<p>No hay discos cargados</p>';
-        } else {
+    } else {
         for (let disco of listadoDiscos) {
             html += disco.mostrarDatos()
+
+            html += `<p>PISTAS BB ${disco.mostrarCantidadPistas()} <p>`
+
+            html += `<p>Duracion total del disco: ${disco.mostrarDuracionTotal()}</p>`
+            html += `<p>Promedio total del disco: ${disco.mostrarPromedioTotalDuracion()}</p>`
+            html += `<p>Promedio total del disco: ${disco.mostrarPistaMayorDuracion()}</p>`
+            
         }
-        html += `<p>cantidad de discos cargados ${listadoDiscos.length}</p>`
+        html += `<p>cantidad de discos cargados ${listadoDiscos.length} </p>`
     }
 
-    
+
     document.getElementById('info').innerHTML = html;
 }
 
@@ -62,13 +69,18 @@ const Buscar = () => {
     //     } while(!(isNaN(id)) )
     //     return id
     // } 
-    if(listadoDiscos.length >= 1) {
+    if (listadoDiscos.length >= 1) {
+        let existeId = true;
         for (let disco of listadoDiscos) {
-            if(disco.getIdDisco() == id) {
+            if (disco.getIdDisco() == id) {
                 html += disco.mostrarDatos()
+                existeId = true
             } else {
-                html += `<p>EL ID INGRESADO NO EXISTE</p>`
+                existeId = false;
             }
+        }
+        if (!existeId) {
+            html += `<p>EL ID INGRESADO NO EXISTE</p>`
         }
     } else {
         html += `<p>NO HAY DISCOS CARGADOS</p>`
@@ -82,8 +94,8 @@ const Buscar = () => {
 function validacionEntradaIdDisco(msg) {
     let num
     do {
-        if(flag) {
-            if(flag == 3) {
+        if (flag) {
+            if (flag == 3) {
                 error.showIntentosSuperados();
                 errorFlag = true
                 num = campoIncompleto;
@@ -92,13 +104,13 @@ function validacionEntradaIdDisco(msg) {
             }
         }
         flag++
-        
+
         num = prompt(msg)
-        if(num == null){
+        if (num == null) {
             break
-        } 
+        }
     } while (!validacionIdExistente(num))
-    
+
     resetError()
     return num
 }
@@ -130,9 +142,9 @@ function validacionEntradaTexto(msg) {
 function validacionEntradaRango(msg) {
     let valor;
     do {
-        if(flag) {
+        if (flag) {
             error.showRangoInvalido();
-            if(flag == 3) {
+            if (flag == 3) {
                 error.showRangoInvalido();
                 errorFlag = true
                 valor = campoIncompleto;
@@ -141,12 +153,12 @@ function validacionEntradaRango(msg) {
             }
         }
         valor = prompt(msg)
-        if(valor == null){
+        if (valor == null) {
             valor = campoIncompleto;
             break
         }
         flag++
-    } while(!(valor > 0 && valor <= 7200))
+    } while (!(valor > 0 && valor <= 7200))
     resetError()
     return valor;
 }
@@ -158,15 +170,15 @@ function resetError() {
 
 function validacionIdExistente(id) {
     let esValido = true;
-    if(isNaN(id)) {
+    if (isNaN(id)) {
         error.showNumeroIncorrecto();
         esValido = false;
-    }  else if (!(id >= 1 && id <= 999)) {
+    } else if (!(id >= 1 && id <= 999)) {
         error.showRangoInvalido();
         esValido = false;
     } else {
         for (let disco of listadoDiscos) {
-            if(disco.getIdDisco() == id) {
+            if (disco.getIdDisco() == id) {
                 error.showCodigoDuplicado()
                 esValido = false
             }
@@ -180,31 +192,75 @@ function Disco() {
     let idDisco = 0
     let nombreDisco = 'El lado oscuro de la let Programación'
     let interprete = 'Los Programadores Anónimos'
-
     let pistas = [];
 
-    this.guardarPista = function(pista) {
+    this.mostrarPistaMayorDuracion = function () {
+        let min = 0;
+        let max = 0;
+        let n = 0;
+        let cont = 0;
+        let pista = '';
+        for (let pista of pistas) {
+            n = pista.getDuracionPista()
+
+            for (let i = 0; i < this.mostrarCantidadPistas(); i++) {
+                if (cont == 0) {
+                    min = n;
+                    max = n;
+                } else {
+                    if(n < min) {
+                        min = n;
+                    }
+                    if(n > max) {
+                        max = n
+                    }
+                }
+            }
+            cont++
+        }
+
+        return `<p>MIN ${min} MAX ${max}</p>`
+    }
+
+    this.mostrarCantidadPistas = function () {
+        return pistas.length;
+    }
+
+    this.guardarPista = function (pista) {
         pistas.push(pista);
     }
 
-    this.mostrarPistas = function() {
+    this.mostrarPistas = function () {
         let m = '';
-		for (let pista of pistas) {
-			m += pista.mostrarDatosPista();
-		};
-	    return m;
+        for (let pista of pistas) {
+            m += `<p> <strong> Nombre de la pista: </strong> ${pista.getNombrePista()} <p/>`
+            let color = ``
+            let msg = `<strong> Duración: </strong> ${pista.getDuracionPista()}`
+
+            let duracion = (pista.getDuracionPista() > 180) ? color = `<p style = "color: red">${msg}</p>` : color = `<p>${msg}</p>`
+            m += duracion;
+        };
+        return m;
     }
 
-    this.mostrarDuracionTotal = function() {
-        //TODO
+    this.mostrarDuracionTotal = function () {
+        let acum = 0
+        for (let pista of pistas) {
+            acum = acum + parseInt(pista.getDuracionPista())
+        }
+        return acum;
     }
 
-    this.mostrarPromedioTotalDuracion = function() {
-        //TODO
+    this.mostrarPromedioTotalDuracion = function () {
+        let cont = 0;
+        for (let i = 0; i < this.mostrarCantidadPistas(); i++) {
+            cont++
+        }
+        return (this.mostrarDuracionTotal() /cont);
     }
 
     //getter y setter idDisco
-    this.setIdDisco = function(id)  {
+    this.setIdDisco = function (id) {
         this.idDisco = id;
     }
     this.getIdDisco = () => {
@@ -212,55 +268,47 @@ function Disco() {
     }
 
     //getter y setter nombreDisco
-    this.setNombreDisco = function(nombre)  {
+    this.setNombreDisco = function (nombre) {
         this.nombreDisco = nombre;
     }
-    this.getNombreDisco = function() {
+    this.getNombreDisco = function () {
         return this.nombreDisco
     }
 
     //getter y setter interprete
-    this.setInterprete = function(id)  {
+    this.setInterprete = function (id) {
         this.interprete = id;
     }
-    this.getInterprete = function() {
+    this.getInterprete = function () {
         return this.interprete
     }
 
-    this.mostrarDatos = function() {
-		let m = `
-			<p><strong>Id disco:</strong> ${this.getIdDisco()} <strong>nombre disco:</strong> ${this.getNombreDisco()} <strong>interprete:</strong> ${this.getInterprete()} <strong>PISTAS BB:</strong> ${this.mostrarPistas()}`;
+    this.mostrarDatos = function () {
+        let m = `
+			<p><strong>Id disco:</strong> ${this.getIdDisco()} <strong>nombre disco:</strong> ${this.getNombreDisco()} <strong>interprete:</strong> ${this.getInterprete()} <strong>PISTAS:</strong> ${this.mostrarPistas()}`;
         return m;
-	}
-    
+    }
+
 }
 
 function Pista() {
     let nombrePista = 'Esa cajita loca llamada variablecita'
     let duracionPista = 200
-    this.mayorDuracionPista = function() {
-        //TODO
-    }
 
     //getter y setter del nombrePista
-    this.setNombrePista = function(nombre)  {
+    this.setNombrePista = function (nombre) {
         this.nombrePista = nombre;
     }
-    this.getNombrePista = function() {
+    this.getNombrePista = function () {
         return this.nombrePista;
     }
 
     //getter y setter del duracionPista
-    this.setDuracionPista = function(duracionPista) {
+    this.setDuracionPista = function (duracionPista) {
         this.duracionPista = duracionPista
     }
-    this.getDuracionPista = function() {
+    this.getDuracionPista = function () {
         return this.duracionPista;
-    }
-
-    this.mostrarDatosPista = () => {
-        let m = `Nombre: ${this.getNombrePista()} - Duracion pista: ${this.getDuracionPista()}`;
-        return m
     }
 }
 
@@ -271,23 +319,23 @@ function ErrorMsg() {
     const intentosSuperados = 'Usted superó la cantidad máxima de intentos'
     const codigoDuplicado = 'El codigo ingresado ya existe'
 
-    this.showNumeroIncorrecto = function() {
+    this.showNumeroIncorrecto = function () {
         alert(numeroIncorrecto);
     }
 
-    this.showTextoInvalido = function() {
+    this.showTextoInvalido = function () {
         alert(textoInvalido)
     }
 
-    this.showRangoInvalido = function() {
+    this.showRangoInvalido = function () {
         alert(rangoInvalido)
     }
 
-    this.showIntentosSuperados = function() {
+    this.showIntentosSuperados = function () {
         alert(intentosSuperados)
     }
 
-    this.showCodigoDuplicado = function() {
+    this.showCodigoDuplicado = function () {
         alert(codigoDuplicado)
     }
 }
